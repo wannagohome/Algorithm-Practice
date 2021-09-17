58a13aa461c8e5c324027e2d4ae74a03adfb4772
wannagohome <iwwuf7@gmail.com>
2021-09-17 20:09:00 +0900

    LeetCdoe 409. Longest Palindrome

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -1,2 +1,9 @@
-package practice;public class Main {
+package practice;
+
+import practice.leetcode.LongestPalindrome;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(LongestPalindrome.solution("abccccdd"));
+    }
 }
--- src/practice/leetcode/LongestPalindrome.java
+++ src/practice/leetcode/LongestPalindrome.java
@@ -1,7 +1,24 @@
-package practice;
+package practice.leetcode;
+
 
 public class LongestPalindrome {
-    public static void main(String[] args) {
-        
+    /**
+     * https://leetcode.com/problems/longest-palindrome/
+     * LeetCdoe 409. Longest Palindrome
+     */
+    public static int solution(String s) {
+        int[] ascii = new int[128];
+        for (char c : s.toCharArray()) {
+            ascii[c]++;
+        }
+
+        int result = 0;
+        for (int i : ascii) {
+            result += i - (i % 2);
+            if (result % 2 == 0 && i % 2 == 1) {
+                result += 1;
+            }
+        }
+        return result;
     }
 }
