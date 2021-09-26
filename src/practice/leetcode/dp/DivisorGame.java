5548bf71c4697f62f9c3385c500f97df951723b8
pete <iwwuf7@gmail.com>
2021-09-26 22:19:37 +0900

    LeetCode 1025. Divisor Game

---
+++ src/practice/leetcode/dp/DivisorGame.java
@@ -0,0 +1,15 @@
+package practice.leetcode.dp;
+
+public class DivisorGame {
+    /**
+     * https://leetcode.com/problems/divisor-game/
+     * LeetCode 1025. Divisor Game
+     *
+     * My Solution : dp문제인데 dpa문제가 아닌 말도 안되는 개똥 문제
+     * result : 0ms
+     */
+
+    public boolean divisorGame(int n) {
+        return n % 2 == 0;
+    }
+}
