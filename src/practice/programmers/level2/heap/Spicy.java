f6bfad299a472db5a4263ffe071c777603202b76
pete <iwwuf7@gmail.com>
2021-10-26 08:34:21 +0900

    Programmers 더 맵게

--- src/practice/programmers/level2/heap/Spicy.java
+++ src/practice/programmers/level2/heap/Spicy.java
@@ -11,6 +11,9 @@ public class Spicy {
      * PriorityQueue, Heap에 대해 배웠다.
      * PriorityQueue를 활용해 작은 값 부터 하나씩 섞는다.
      *
+     * scoville을 sorting한 ArrayList를 사용해봤으나
+     * ArrayList는 삭제가 N이므로 효율성 테스트를 통과하지 못한다.
+     *
      * Time Complexity : O(NlogN)
      */
 
