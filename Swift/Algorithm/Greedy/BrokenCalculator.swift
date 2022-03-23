7113d904a4df5cf71efcd27840d271a7e0328144
pete <iwwuf7@gmail.com>
2022-03-23 15:28:51 +0900

    LeetCode 991. Broken Calculator

---
+++ Swift/Algorithm/Greedy/BrokenCalculator.swift
@@ -0,0 +1,16 @@
+//
+//  BrokenCalculator.swift
+//  Algorithm
+//
+//  Created by CW on 2022/03/23.
+//
+
+import Foundation
+
+struct BrokenCalculator {
+    func brokenCalc(_ startValue: Int, _ target: Int) -> Int {
+        if startValue >= target { return startValue - target }
+        if target % 2 == 0 { return 1 + brokenCalc(startValue, target / 2) }
+        return 1 + brokenCalc(startValue, target + 1)
+    }
+}
