7f95667bb49f1df3953a52f5a1ee715fe724a4f5
wannagohome <iwwuf7@gmail.com>
2024-02-19 20:20:31 +0900

    LeetCode 231. Power of Two

---
+++ src/practice/PowerOfTwo.java
@@ -0,0 +1,21 @@
+package practice;
+
+
+/**
+ * LeetCode 231. Power of Two
+ */
+public class PowerOfTwo {
+
+	public boolean isPowerOfTwo(int n) {
+		if (n <= 0) {
+			return false;
+		}
+		while (n != 1) {
+			if (n % 2 != 0) {
+				return false;
+			}
+			n /= 2;
+		}
+		return true;
+	}
+}
